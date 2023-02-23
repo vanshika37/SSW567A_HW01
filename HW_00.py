79cbbf072425b58e962f4c55c94c1bbacaced997
vanshika37 <vanshika16276@iiitd.ac.in>
2023-02-22 23:16:03 -0500

    Add files via upload

---
+++ HW_00.py
@@ -0,0 +1,12 @@
+from datetime import date
+
+def my_brand(assignmentname):
+	print("=*=*=*= Vanshika Chopra =*=*=*=")
+	print("=*=*=*=", assignmentname, "=*=*=*=") 	
+	print("=*=*=*= HW 00 - Tools Setup =*=*=*=")
+	dates= date.today()
+	print( "=*=*=*=",dates,"=*=*=*=")
+	print("hello world")
+
+if __name__ == '__main__':
+    my_brand("HW00")
