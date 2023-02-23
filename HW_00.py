25a8023a8748c0f89f77d38b0b06958f4c96f92f
vanshika37 <vanshika16276@iiitd.ac.in>
2023-02-22 23:24:58 -0500

    Update HW_00.py

--- HW_00.py
+++ HW_00.py
@@ -6,7 +6,8 @@ def my_brand(assignmentname):
 	print("=*=*=*= HW 00 - Tools Setup =*=*=*=")
 	dates= date.today()
 	print( "=*=*=*=",dates,"=*=*=*=")
-	print("hello world")
 
 if __name__ == '__main__':
     my_brand("HW00")
+    print("hello world")
+
